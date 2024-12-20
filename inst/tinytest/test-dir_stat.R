eec07c08f067762394c338521315d49f71a41370
wlandau <will.landau.oss@gmail.com>
2024-12-20 11:57:51 -0500

    coverage

--- inst/tinytest/test-dir_stat.R
+++ inst/tinytest/test-dir_stat.R
@@ -1,37 +1,78 @@
 local({
-  for (units_mtime in c("POSIXct", "numeric")) {
+  if (tolower(Sys.info()["sysname"]) != "windows") {
+    for (units_mtime in c("POSIXct", "numeric")) {
+      path <- tempfile()
+      dir.create(path)
+      writeLines("line", file.path(path, "a"))
+      writeLines("line2", file.path(path, "b"))
+      dir.create(file.path(path, "dir"))
+      file.create(file.path(path, "dir", "x"))
+      out_c <- dir_stat(
+        path,
+        method = "c",
+        units_size = "bytes",
+        units_mtime = units_mtime
+      )
+      out_r <- dir_stat(
+        path,
+        method = "r",
+        units_size = "bytes",
+        units_mtime = units_mtime
+      )
+      out_c$path <- basename(out_c$path)
+      out_r$path <- basename(out_r$path)
+      out_c$size <- as.integer(out_c$size)
+      out_r$size <- as.integer(out_r$size)
+      expect_equal(out_c, out_r)
+      expect_equal(as.numeric(out_c$mtime) - as.numeric(out_r$mtime), c(0, 0))
+      expect_equal(
+        as.character(.POSIXct(out_c$mtime)),
+        as.character(.POSIXct(out_r$mtime))
+      )
+      expect_equal(out_c$size[out_c$path == "a"], 5L)
+      expect_equal(out_c$size[out_c$path == "b"], 6L)
+      expect_equal(sort(colnames(out_c)), sort(c("path", "size", "mtime")))
+      expect_equal(nrow(out_c), 2L)
+      unlink(path, recursive = TRUE)
+    }
+  }
+})
+
+local({
+  if (tolower(Sys.info()["sysname"]) != "windows") {
     path <- tempfile()
     dir.create(path)
     writeLines("line", file.path(path, "a"))
     writeLines("line2", file.path(path, "b"))
-    dir.create(file.path(path, "dir"))
-    file.create(file.path(path, "dir", "x"))
-    out_c <- dir_stat(
+    out <- dir_stat(
       path,
       method = "c",
       units_size = "bytes",
-      units_mtime = "numeric"
+      units_mtime = "numeric",
+      recent = as.difftime(1, units = "hours")
     )
-    out_r <- dir_stat(
+    out$path <- basename(out$path)
+    expect_equal(nrow(out), 2L)
+    expect_equal(out$size[out$path == "a"], 5L)
+    expect_equal(out$size[out$path == "b"], 6L)
+    expect_true(is.numeric(out$mtime))
+    unlink(path, recursive = TRUE)
+  }
+})
+
+local({
+  if (tolower(Sys.info()["sysname"]) != "windows") {
+    path <- tempfile()
+    dir.create(path)
+    out <- dir_stat(
       path,
-      method = "r",
+      method = "c",
       units_size = "bytes",
-      units_mtime = "numeric"
+      units_mtime = "numeric",
+      recent = as.difftime(1, units = "hours")
     )
-    out_c$path <- basename(out_c$path)
-    out_r$path <- basename(out_r$path)
-    out_c$size <- as.integer(out_c$size)
-    out_r$size <- as.integer(out_r$size)
-    expect_equal(out_c, out_r)
-    expect_equal(as.numeric(out_c$mtime) - as.numeric(out_r$mtime), c(0, 0))
-    expect_equal(
-      as.character(.POSIXct(out_c$mtime)),
-      as.character(.POSIXct(out_r$mtime))
-    )
-    expect_equal(out_c$size[out_c$path == "a"], 5L)
-    expect_equal(out_c$size[out_c$path == "b"], 6L)
-    expect_equal(sort(colnames(out_c)), sort(c("path", "size", "mtime")))
-    expect_equal(nrow(out_c), 2L)
+    expect_equal(nrow(out), 0L)
+    expect_equal(sort(colnames(out)), sort(c("path", "size", "mtime")))
     unlink(path, recursive = TRUE)
   }
 })
